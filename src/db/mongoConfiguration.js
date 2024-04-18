8991e2273e51010c560d9b045fc988cac3c1f62b
Cristian Torres <cristian.torres19@hotmail.com>
2024-04-17 23:41:21 -0500

    base de datos local

--- src/db/mongoConfiguration.js
+++ src/db/mongoConfiguration.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config();
 
+async function connectLocally() {
+    const dbName = 'planemerg' // solo cambiar este string si se requiere cambiar base de datos
+    const connectionString = `mongodb://localhost:27017/${dbName}`;  // Database name is still 'SAGNIRIB'
+    try {
+        await mongoose.connect(connectionString);
+        console.log('💾 Connected to MongoDB on localhost 📚📚📚📚📚📚📚📚📚📚');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+    }
+}
+
 async function connectMongoDB() {
     const username = process.env.DB_USERNAME
+    if (!username) {
+        console.log("🚀 intentando conectarse localmente a la bae de datos 😽 >>")
+        await connectLocally()
+        return
+    }
     const password = process.env.DB_PASSWORD
     const mongoHost = process.env.DB_HOST
     const connectionString = `mongodb+srv://${username}:${password}@${mongoHost}/SAGNIRIB`
